2a7b3b10f9fd12a1b444fab725865e459bf66928
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-01-12 17:07:08 +0530

    Image and Video Sensitivity analysing

--- ChartDemo/ChartDemo/SensitivityAnalysisDemo.swift
+++ ChartDemo/ChartDemo/SensitivityAnalysisDemo.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import SwiftUI
+import SensitiveContentAnalysis
+
+struct SensitiveAnalysisView: View {
+    var body: some View{
+        VStack{
+            Text("Sensiive Content Analysis Demo").foregroundStyle(.orange)
+//            Image("ranveer", label: Text("Ranveer"))
+                
+        }.padding(10)
+            
+    }
+    
+    func analyse(imageName: String){
+        let analyser = SCSensitivityAnalyzer()
+        let policy = analyser.analysisPolicy
+        if policy == .disabled{
+            return
+        }
+        
+        //For video
+//        let handler = analyser.videoAnalysis(forFileAt: localvideoFileUrl)
+//        let response = try await hanlder.hasSensitiveContent()
+        
+        //For Image
+        
+        guard let sensitiveContentImage = UIImage(named: "ranveer")
+//                let response = try await analyser.analyzeImage(sensitiveContentImage.cgImage)
+        
+    }
+}
+
+#Preview {
+    SensitiveAnalysisView()
+}
