e43a303fc1ca50c0fb8be31de6904d504a979fb3
Anand Upadhyay <anandgupadhyay03@gmail.com>
2024-01-22 04:12:09 +0530

    Remove Navigation Link

--- ChartDemo/ChartDemo/SensitivityAnalysisDemo.swift
+++ ChartDemo/ChartDemo/SensitivityAnalysisDemo.swift
@@ -95,18 +95,7 @@ struct SensitiveAnalysisView: View {
         //                await analyse(imageName: imageName)
         //            }
     }
-    NavigationLink {
-        // destination view to navigation to
-        DetailView()
-    } label: {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+    
 }
     
     func checkSensitivity(imageName: String)async{
@@ -172,3 +161,17 @@ struct DetailView: View {
     }
 }
 
+/*
+ NavigationLink {
+     // destination view to navigation to
+     DetailView()
+ } label: {
+     VStack {
+         Image(systemName: "globe")
+             .imageScale(.large)
+             .foregroundColor(.accentColor)
+         Text("Hello, world!")
+     }
+     .padding()
+ }
+ */
